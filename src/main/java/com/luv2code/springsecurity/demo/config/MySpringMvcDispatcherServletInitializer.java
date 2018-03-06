0ba10e6175c1324fda7473d49400c71b1a30a0b8
Tarık Nural <tariknural00@gmail.com>
2018-03-06 15:05:16 +0300

    descp. added.

--- src/main/java/com/luv2code/springsecurity/demo/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/MySpringMvcDispatcherServletInitializer.java
@@ -2,6 +2,9 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+// dispatcher servlet configurations super class is configured by spring framework.
+// we OVERRIDING its configuration. 
+
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
@@ -10,6 +13,7 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
 		return null;
 	}
 
+	
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		return new Class[] { DemoAppConfig.class };
@@ -21,6 +25,22 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
 	}
 
 }
+
+
+/*
+ * <servlet>
+    <servlet-name>dispatcher</servlet-name>
+    <servlet-class>org.springframework.web.servlet.DispatcherServlet</servlet-class>
+    <init-param>
+      <param-name>contextConfigLocation</param-name>
+      <param-value>/WEB-INF/spring-mvc-crud-demo-servlet.xml</param-value>
+    </init-param>
+    <load-on-startup>1</load-on-startup>
+  </servlet>
+  
+  org.springframework.web.servlet.DispatcherServlet -- > this was spring dispatcher servlet class
+  init param tags was overriding and re-configuring it. its the same in here.
+ */
 
 
 
